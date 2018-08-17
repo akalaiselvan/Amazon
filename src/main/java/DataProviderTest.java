bdc1de1f1bff06ec3cb6eb7a9b7849fbc28f4c36
KalaiSelvan.A <kalaiselvan.a@gofrugal.com>
2018-08-17 15:53:33 +0530

    removed

--- src/main/java/DataProviderTest.java
+++ src/main/java/DataProviderTest.java
@@ -10,26 +10,6 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class DataProviderTest {
-    Selenium selenium=new Selenium();
-    TestUtils utils=new TestUtils();
-
-    public DataProviderTest() throws IOException, BiffException {
-    }
-
-    @DataProvider(name = "login")
-    public Object[][] testcredential() throws Exception {
-    return TestHelper.getfromExcel(utils.SHEET_LOCATIO,utils.sheetname);
-    }
-
-    @Test(dataProvider = "login")
-    public void setup(String username , String password) throws InterruptedException, IOException {
-        //selenium.login(username,password);
-
-    }
-    public void verifytext(){
-    }
-
-
 
 
 }
